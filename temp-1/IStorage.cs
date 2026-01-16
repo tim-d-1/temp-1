7732112c9df5e91e4f1936b9a72b39a6c774efd3
tim <tim.donin.dev@gmail.com>
2026-01-16 16:01:04 +0200

    IStorage

---
+++ temp-1/IStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp_1
+{
+    internal interface IStorage
+    {
+        public Task WriteAsync(string data);
+
+        public Task<string> ReadAsync();
+    }
+}
