104429e2cccd365333a94d1276a17f3edda4cad2
tim <tim.donin.dev@gmail.com>
2026-01-16 16:08:51 +0200

    done

--- temp-1/JsonStorage.cs
+++ temp-1/JsonStorage.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
 
 namespace temp_1;
 
 internal class JsonStorage : IStorage
 {
+    public async Task WriteAsync(string data)
+    {
+        using FileStream SourceStream = File.Open("data.json", FileMode.Open);
+
+        await JsonSerializer.SerializeAsync(SourceStream, data);
+
+    }
 }
